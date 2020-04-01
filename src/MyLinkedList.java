7ce1d02236860a074e7ec7425b9dabae9c2abc17
vanduc2514 <vanduc2514@gmail.com>
2020-04-01 15:15:06 +0700

    Add Test

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -6,11 +6,12 @@ public class MyLinkedList {
         head = new Node(data);
     }
 
+
     private class Node {
         private Node next;
         private Object data;
 
-        public Node (Object data) {
+        public Node(Object data) {
             this.data = data;
         }
 
@@ -19,11 +20,11 @@ public class MyLinkedList {
         }
     }
 
-    public void add (int index, Object data) {
+    public void add(int index, Object data) {
         Node temp = head;
         Node holder;
 
-        for (int i =0; i < index -1 && temp.next != null; i++ ) {
+        for (int i = 0; i < index - 1 && temp.next != null; i++) {
             temp = temp.next;
         }
 
@@ -31,5 +32,28 @@ public class MyLinkedList {
         temp.next = new Node(data);
         temp.next.next = holder;
         numNodes++;
+    }
+
+    public void addFirst(Object data) {
+        Node temp = head;
+        head = new Node(data);
+        head.next = temp;
+        numNodes++;
+    }
+
+    public Node get(int index) {
+        Node temp = head;
+        for (int i =0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     }
 }
---
+++ test/MyLinkedListTest.java
@@ -0,0 +1,12 @@
+class MyLinkedListTest {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList(10);
+           myLinkedList.addFirst(11);
+           myLinkedList.addFirst(12);
+           myLinkedList.addFirst(13);
+
+           myLinkedList.add(4,9);
+           myLinkedList.add(4,9);
+           myLinkedList.printList();
+    }
+}
