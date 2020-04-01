1eef24e3640646b39d62a3d192ff1a7e799c555a
vanduc2514 <vanduc2514@gmail.com>
2020-04-01 23:48:29 +0700

    Add Method

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,15 +1,30 @@
 public class MyLinkedList {
     private int numNodes;
     private Node head;
+    private Node tail;
 
     public MyLinkedList(Object data) {
         head = new Node(data);
+        tail = head;
+        numNodes++;
     }
 
+    public int getNumNodes() {
+        return numNodes;
+    }
 
-    private class Node {
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public static class Node {
         private Node next;
         private Object data;
+        private int index;
 
         public Node(Object data) {
             this.data = data;
@@ -18,32 +33,55 @@ public class MyLinkedList {
         public Object getData() {
             return this.data;
         }
+
+        public void setData(Object data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
 
     public void add(int index, Object data) {
-        Node temp = head;
-        Node holder;
-
-        for (int i = 0; i < index - 1 && temp.next != null; i++) {
-            temp = temp.next;
+        if (index == 0) {
+            addFirst(data);
+        } else if (index == numNodes) {
+            addLast(data);
+        } else {
+            Node newNode = new Node(data);
+            Node current = head;
+            for (int nodeIndex = 1; nodeIndex < index; nodeIndex++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+            numNodes++;
         }
 
-        holder = temp.next;
-        temp.next = new Node(data);
-        temp.next.next = holder;
-        numNodes++;
     }
 
     public void addFirst(Object data) {
-        Node temp = head;
-        head = new Node(data);
-        head.next = temp;
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+        numNodes++;
+    }
+
+    public void addLast(Object data) {
+        Node newNode = new Node(data);
+        tail.next = newNode;
+        tail = newNode;
         numNodes++;
     }
 
     public Node get(int index) {
         Node temp = head;
-        for (int i =0; i < index; i++) {
+        for (int i = 0; i < index; i++) {
             temp = temp.next;
         }
         return temp;
@@ -55,5 +93,62 @@ public class MyLinkedList {
             System.out.println(temp.data);
             temp = temp.next;
         }
+    }
+
+    public void remove(int index) {
+        if (index == 0) {
+            removeFirst();
+        } else if (index == numNodes) {
+            removeLast();
+        } else {
+            Node current = head;
+            for (int indexNode = 1; indexNode < index; indexNode++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+    }
+
+    public void removeFirst() {
+        head = head.next;
+    }
+
+    public void removeLast() {
+        Node current = head;
+        for (int indexNode = 1; indexNode < numNodes - 1; indexNode++) {
+            current = current.next;
+        }
+        tail = current;
+        tail.next = null;
+    }
+
+    public boolean removeObject(Object data) {
+        if (isContains(data)) {
+            Node current = head;
+            for (int index = 0; index < numNodes; index++) {
+                if (current.getData() == data) {
+                    remove(index);
+                    return true;
+                }
+                current = current.next;
+            }
+        }
+        return false;
+    }
+
+    public boolean isContains(Object data) {
+        Node current = head;
+        for (int index = 0; index < numNodes; index++) {
+            if (current.getData() == data) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public int indexOf(Object data) {
+        Node current = head;
+        throw new UnsupportedOperationException();
     }
 }
--- test/MyLinkedListTest.java
+++ test/MyLinkedListTest.java
@@ -1,12 +1,14 @@
 class MyLinkedListTest {
     public static void main(String[] args) {
         MyLinkedList myLinkedList = new MyLinkedList(10);
-           myLinkedList.addFirst(11);
-           myLinkedList.addFirst(12);
-           myLinkedList.addFirst(13);
-
-           myLinkedList.add(4,9);
-           myLinkedList.add(4,9);
-           myLinkedList.printList();
+        System.out.println(myLinkedList.getHead());
+        MyLinkedList.Node node = myLinkedList.getHead();
+        System.out.println(node.getNext());
+        myLinkedList.addFirst(11);
+        myLinkedList.addFirst(12);
+        myLinkedList.addFirst(13);
+        myLinkedList.add(4, 9);
+        myLinkedList.add(4, 9);
+        myLinkedList.printList();
     }
 }
